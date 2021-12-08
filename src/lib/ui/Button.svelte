fa118de9055346048cb104b639b56f53ddeee90a
Vu Nguyen <nxvu699134@gmail.com>
2021-12-09 04:47:16 +0700

    Run linter

--- src/lib/ui/Button.svelte
+++ src/lib/ui/Button.svelte
@@ -1,56 +1,56 @@
 <script>
-  import { createEventDispatcher } from "svelte";
-  let outsideClass = "";
-  export { outsideClass as class };
+	import { createEventDispatcher } from 'svelte';
+	let outsideClass = '';
+	export { outsideClass as class };
 
-  let size = "md-size";
+	let size = 'md-size';
 
-  const className = `${size} ${outsideClass}`;
+	const className = `${size} ${outsideClass}`;
 
-  const dispatch = createEventDispatcher();
-  const onClick = () => dispatch("click");
+	const dispatch = createEventDispatcher();
+	const onClick = () => dispatch('click');
 </script>
 
 <button class={className} on:click={onClick}>
-  <slot />
+	<slot />
 </button>
 
 <style>
-  button {
-    background-color: var(--color-main-bg);
-    transition: all 0.2s ease-in-out;
-    outline: 0;
-    border-radius: var(--border-radius);
-    border: 1px solid var(--color-border);
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: var(--color-text);
-    box-shadow: var(--shadow-outset-200);
-    cursor: pointer;
-  }
+	button {
+		background-color: var(--color-main-bg);
+		transition: all 0.2s ease-in-out;
+		outline: 0;
+		border-radius: var(--border-radius);
+		border: 1px solid var(--color-border);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		color: var(--color-text);
+		box-shadow: var(--shadow-outset-200);
+		cursor: pointer;
+	}
 
-  button:not(:disabled):active {
-    box-shadow: var(--shadow-inset-200);
-  }
+	button:not(:disabled):active {
+		box-shadow: var(--shadow-inset-200);
+	}
 
-  button:focus {
-    outline: 0;
-  }
+	button:focus {
+		outline: 0;
+	}
 
-  button.md-size {
-    padding: 0.375rem 0.5rem;
-    font-size: 1rem;
-  }
+	button.md-size {
+		padding: 0.375rem 0.5rem;
+		font-size: var(--font-size-400);
+	}
 
-  button.lg-size {
-    padding: 0.5rem 1rem;
-    font-size: 1.25rem;
-  }
+	button.lg-size {
+		padding: 0.5rem 1rem;
+		font-size: var(--font-size-500);
+	}
 
-  button.sm-size {
-    padding: 0.25rem 0.5rem;
-    font-size: 0.875rem;
-    font-weight: normal;
-  }
+	button.sm-size {
+		padding: 0.25rem 0.5rem;
+		font-size: var(--font-size-300);
+		font-weight: normal;
+	}
 </style>
