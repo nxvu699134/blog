db50cc098df43a61384db189d50f0e56f698e60d
Vu Nguyen <nxvu699134@gmail.com>
2021-12-12 03:34:02 +0700

    Config mdsvex with example
    config
    
    tes

---
+++ mdsvex.config.js
@@ -0,0 +1,7 @@
+export default {
+	layout: {
+		// _: './src/lib/layouts/post.svelte',
+		post: './src/lib/layouts/post.svelte'
+	},
+	extensions: ['.svx']
+};
---
+++ src/lib/layouts/post.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	// TODO: This layout is used for SEO Tag
+	export let title = '';
+</script>
+
+<div>??{title}</div>
+<slot />
