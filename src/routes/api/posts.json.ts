54c977ebe412c2198e5c855114d66785059979a1
Vu Nguyen <nxvu699134@gmail.com>
2021-12-20 04:45:45 +0700

    Remove redundant

--- src/routes/api/posts.json.ts
+++ src/routes/api/posts.json.ts
@@ -5,7 +5,6 @@ const Header = {
 	'content-type': 'application/json'
 };
 export const get: RequestHandler = async ({ query }): Promise<EndpointOutput> => {
-	// request.locals.userid comes from src/hooks.js
 	let postNames = await getAllPostNames();
 
 	const limit = parseInt(query.get('limit'));
