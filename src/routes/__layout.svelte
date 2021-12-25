8a4e90d8e7c72c74d6c653e8499e8e8fd47beb08
Vu Nguyen <nxvu699134@gmail.com>
2021-12-26 04:52:51 +0700

    Use scss

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte';
-	import '$lib/css/global.css';
-	import '$lib/css/fonts.css';
-	import '$lib/css/theme.css';
+	import '$lib/css/global.scss';
 </script>
 
 <Header />
 <main>
 	<slot />
 </main>
-
-<style>
-</style>
