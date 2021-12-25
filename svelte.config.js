a0f536f410033c33bf35e7362cf977a29b691d3c
Vu Nguyen <nxvu699134@gmail.com>
2021-12-26 04:14:39 +0700

    Setup postcss

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,7 @@ import preprocess from 'svelte-preprocess';
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: [mdsvex(mdsvexConfig), preprocess()],
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
 
