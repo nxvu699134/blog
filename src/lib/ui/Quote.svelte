22b3a68714c52effb85150298e046002789ab264
Vu Nguyen <nxvu699134@gmail.com>
2021-12-28 19:31:37 +0700

    Add profile description

---
+++ src/lib/ui/Quote.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	let outsideClass = '';
+	export { outsideClass as class };
+	export let author: string = '';
+</script>
+
+<blockquote class={`${outsideClass}`}>
+	<i class="fas fa-quote-left quotetick" />
+	<slot />
+	<i class="fas fa-quote-right quotetick" />
+	<br />
+	{#if author}
+		<cite>{author}</cite>
+	{/if}
+</blockquote>
+
+<style lang="scss">
+	blockquote {
+		font-family: var(--font-heading);
+		font-size: var(--font-size-500);
+		padding-left: var(--spacing-500);
+		border-left: 6px solid var(--color-primary-400);
+		font-style: italic;
+		color: var(--color-grey-700);
+		margin: var(--spacing-400) var(--spacing-600);
+
+		i.quotetick {
+			color: var(--color-grey-200);
+		}
+	}
+
+	cite {
+		font-family: var(--font-article);
+		font-size: var(--font-size-300);
+		font-style: normal;
+		color: var(--color-grey-500);
+
+		&:before {
+			content: '\2015';
+			margin-right: var(--spacing-200);
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -19,6 +19,7 @@
 
 <script lang="ts">
 	import LandingButton from '$lib/ui/LandingButton.svelte';
+	import Quote from '$lib/ui/Quote.svelte';
 </script>
 
 <div class="banner">
@@ -28,7 +29,27 @@
 	</h4>
 	<div class="action-container">
 		<LandingButton href="/blog" leftIcon="fas fa-pencil-alt">Blog</LandingButton>
-		<LandingButton href="/about" leftIcon="fas fa-pencil-alt" outline>About</LandingButton>
+		<LandingButton href="/about" leftIcon="fas fa-user-secret" outline>About</LandingButton>
+	</div>
+</div>
+
+<div class="profile">
+	<Quote class="public-quote" author="Abathur">
+		Never perfect. Perfection goal that changes. Never stops moving. Can chase, cannot catch.
+	</Quote>
+
+	<div class="description">
+		<div class="content">
+			<h4>I'm a Full-stuck developer, who aim to build things that perfect</h4>
+			<p>
+				Maybe I just looks like a cat chasing my damn "perfection" tail. I'm a Vimmer also a
+				Starcraft player. Feel free to contact me!<br />
+				Have a nice day. 🍺
+			</p>
+		</div>
+		<div class="avatar-container">
+			<img src="/assets/imgs/avatar.png" alt="avatar" />
+		</div>
 	</div>
 </div>
 
@@ -36,16 +57,18 @@
 	@use '../lib/css/responsive.scss' as *;
 
 	.banner {
-		color: var(--color-primary-800);
-		margin-top: var(--spacing-800);
+		margin-top: var(--spacing-600);
+		margin-bottom: var(--spacing-900);
 	}
 
 	.greeting {
 		text-align: center;
+		color: var(--color-primary-800);
 	}
 
 	.quote {
 		margin-top: var(--spacing-400);
+		color: var(--color-primary-700);
 		text-align: center;
 	}
 
@@ -57,7 +80,32 @@
 		margin-top: var(--spacing-600);
 	}
 
+	.profile {
+		display: flex;
+		flex-flow: column;
+		align-items: center;
+		margin-top: var(--spacing-400);
+
+		.description {
+			display: flex;
+			flex-flow: column;
+			align-items: center;
+			margin: var(--spacing-400) var(--spacing-200);
+		}
+
+		.avatar-container {
+			margin-top: var(--spacing-400);
+			img {
+				height: 96px;
+			}
+		}
+	}
+
 	@include for-tablet-and-desktop {
+		.banner {
+			margin-top: var(--spacing-800);
+		}
+
 		.action-container {
 			flex-flow: row;
 			justify-content: center;
