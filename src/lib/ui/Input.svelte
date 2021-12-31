3dd4a3bc831bd4af0987ee1432f8f91b01337e54
Vu Nguyen <nxvu699134@gmail.com>
2022-01-01 05:20:48 +0700

    Restyle input group

--- src/lib/ui/Input.svelte
+++ src/lib/ui/Input.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { colorScheme } from '$lib/states/global';
 	/* export let label: string = ''; */
 	export let preIcon: string = '';
 	export let postIcon: string = '';
@@ -7,18 +8,13 @@
 	export { outsideClass as class };
 </script>
 
-<div class={`input-group soft-transition ${outsideClass}`}>
+<div class={`input-group ${outsideClass}`} class:dark={$colorScheme === 'dark'}>
 	{#if preIcon}
 		<span class="pre-icon">
 			<i class={preIcon} />
 		</span>
 	{/if}
-	<input
-		bind:value
-		class="soft-down-200"
-		class:has-pre-icon={preIcon}
-		class:has-post-icon={postIcon}
-	/>
+	<input bind:value />
 	{#if postIcon}
 		<span class="post-icon">
 			<i class={postIcon} />
@@ -26,16 +22,27 @@
 	{/if}
 </div>
 
-<style>
+<style lang="scss">
 	.input-group {
 		display: flex;
-		align-items: stretch;
 		border-radius: var(--border-radius);
-		border: 1px solid var(--color-border);
+		border: 1px solid var(--color-grey-200);
+		overflow: hidden;
+		box-shadow: 0 0 0 10px transparent;
+		transition: box-shadow 0.2s ease-out;
+
+		&:focus-within {
+			box-shadow: 0 0 4px 2px var(--color-primary-200);
+			border-color: var(--color-primary-300);
+		}
 	}
 
-	.input-group:focus-within {
-		box-shadow: var(--shadow-outset-200);
+	.input-group.dark {
+		border-color: var(--color-grey-700);
+		&:focus-within {
+			box-shadow: 0 0 4px 2px var(--color-primary-900);
+			border-color: var(--color-primary-700);
+		}
 	}
 
 	.input-group input {
@@ -44,36 +51,24 @@
 		color: var(--color-text);
 		font-size: var(--font-size-400);
 		padding: var(--spacing-200) var(--spacing-300);
-		border-radius: var(--border-radius);
 		line-height: 1.5;
 		flex: 1;
-	}
 
-	.input-group input.has-pre-icon {
-		border-top-left-radius: 0;
-		border-bottom-left-radius: 0;
-	}
-
-	.input-group input.has-post-icon {
-		border-top-right-radius: 0;
-		border-bottom-right-radius: 0;
-	}
-
-	.input-group input:focus {
-		outline: 0;
+		&:focus {
+			outline: 0;
+		}
 	}
 
 	.input-group .pre-icon,
 	.input-group .post-icon {
 		padding: var(--spacing-200) var(--spacing-300);
 		font-size: var(--font-size-400);
+		background-color: var(--color-primary-400);
+		color: white;
 	}
 
-	.input-group .pre-icon {
-		border-right: 1px solid var(--color-border);
-	}
-
-	.input-group .post-icon {
-		border-left: 1px solid var(--color-border);
+	.input-group.dark .pre-icon,
+	.input-group.dark .post-icon {
+		background-color: var(--color-primary-600);
 	}
 </style>
