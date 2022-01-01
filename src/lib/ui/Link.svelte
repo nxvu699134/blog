bf54346e6baef6a3d296d7ef1ccdb453bfbde4e3
Vu Nguyen <nxvu699134@gmail.com>
2022-01-02 00:44:35 +0700

    Add Link component

---
+++ src/lib/ui/Link.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let href: string = undefined;
+</script>
+
+<a {href}>
+	<slot />
+</a>
+
+<style lang="scss">
+	a {
+		color: var(--color-text-primary);
+		border-bottom: 2px solid var(--color-text-primary);
+	}
+</style>
+
