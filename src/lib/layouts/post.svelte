f26f63e1f367a04c2d2112702443034fe2968b63
Vu Nguyen <nxvu699134@gmail.com>
2022-01-01 22:06:46 +0700

    Add card to post layout

--- src/lib/layouts/post.svelte
+++ src/lib/layouts/post.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
+	import Card from '$lib/ui/Card.svelte';
 	// TODO: This layout is used for SEO Tag
-	export let title = '';
 </script>
 
-<div>??{title}</div>
-<slot />
+<Card class="mdsvex-layout">
+	<slot />
+</Card>
+
+<style lang="scss">
+	:global(.mdsvex-layout) {
+		margin-top: var(--spacing-600);
+	}
+</style>
