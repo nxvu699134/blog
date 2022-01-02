8c65dd18d5bfe58b12427c83590f5a47c6c7ee4c
Vu Nguyen <nxvu699134@gmail.com>
2022-01-02 18:43:02 +0700

    Change style of link

--- src/lib/ui/Link.svelte
+++ src/lib/ui/Link.svelte
@@ -9,7 +9,12 @@
 <style lang="scss">
 	a {
 		color: var(--color-text-primary);
-		border-bottom: 2px solid var(--color-text-primary);
+		/* border-bottom: 2px solid var(--color-text-primary); */
+		box-shadow: inset 0 -3px 0 var(--color-text-primary-disable);
+		transition: background-color 0.2s ease-in-out;
+
+		&:hover {
+			background-color: var(--color-text-primary-disable);
+		}
 	}
 </style>
-
