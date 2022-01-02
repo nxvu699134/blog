9695c901fba07c8239a627d110aa5c088c2e9bcc
Vu Nguyen <nxvu699134@gmail.com>
2022-01-02 17:51:22 +0700

    Use debounce for search input

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -14,17 +14,28 @@
 	import PostCard from '$lib/components/PostCard.svelte';
 	import CardsLayout from '$lib/components/CardsLayout.svelte';
 	import Section from '$lib/ui/Section.svelte';
+	import { debounce } from '$lib/utils/debounce';
 
 	export let posts: IPostMeta[] = [];
+	let filteredPosts = posts;
 	let searchText: string;
+	const onSearch = debounce((query: string) => {
+		if (query) {
+			filteredPosts = posts.filter((p) => p.title.toLowerCase().includes(query.toLowerCase()));
+		} else {
+			filteredPosts = posts;
+		}
+	}, 500);
+
+	$: onSearch(searchText);
 </script>
 
 <Section title="all posts">
 	<div class="search-bar">
-		<Input preIcon="fas fa-search" value={searchText} />
+		<Input preIcon="fas fa-search" bind:value={searchText} />
 	</div>
 	<CardsLayout>
-		{#each posts as post}
+		{#each filteredPosts as post}
 			<PostCard data={post} />
 		{/each}
 	</CardsLayout>
