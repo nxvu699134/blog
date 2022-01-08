862fce4c92eb06b38f0f8840364d29ecfe67c4c8
Vu Nguyen <nxvu699134@gmail.com>
2022-01-08 23:31:41 +0700

    Change about to resume

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,7 +30,7 @@
 	</p>
 	<div class="action-container">
 		<LandingButton href="/blog" leftIcon="fas fa-pencil-alt">Blog</LandingButton>
-		<LandingButton href="/about" leftIcon="fas fa-user-secret" outline>About</LandingButton>
+		<LandingButton href="/resume" leftIcon="fas fa-user-secret" outline>My Résumé</LandingButton>
 	</div>
 </div>
 
