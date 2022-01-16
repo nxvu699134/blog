99c890ba9952a2fdb3c82398d558dcba1c5290b3
Vu Nguyen <nxvu699134@gmail.com>
2022-01-16 19:07:54 +0700

    Add page transition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,8 +1,18 @@
+<script context="module" lang="ts">
+	export const load = ({ url }) => ({
+		props: {
+			key: url.pathname
+		}
+	});
+</script>
+
 <script lang="ts">
+	import { fly } from 'svelte/transition';
 	import Header from '$lib/components/Header.svelte';
 	import WaveBackground from '$lib/components/WaveBackground.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '$lib/css/global.scss';
+	export let key: string;
 </script>
 
 <svelte:head>
@@ -14,7 +24,9 @@
 
 <Header />
 <WaveBackground />
-<main>
-	<slot />
-</main>
+{#key key}
+	<main in:fly={{ x: -16, duration: 200, delay: 200 }} out:fly={{ x: 16, duration: 200 }}>
+		<slot />
+	</main>
+{/key}
 <Footer />
